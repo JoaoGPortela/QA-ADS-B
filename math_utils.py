b29dc2b2967f4236671b8211fdb3d681a9b602ea
Breno-Feitoza <feitozabreno1@gmail.com>
2025-04-16 20:52:27 -0300

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -8,5 +8,5 @@ def subtract(a, b):
 def multiply(a, b):
   if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
     raise ValueError("Both arguments must be numbers")
-    return a * b
+  return a * b
 
